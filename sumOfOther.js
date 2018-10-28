10bd977eaff5dd48f1c90241f4484401bed01707
vovakor1 <v.korotkin111gmail.com>
2018-10-28 20:33:28 +0300

    refactor: delete parentheses in error message

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,7 +1,7 @@
 function sumOfOther(array) {
     let result = [];
     for (let i = 0; i < array.length; i++){
-        if(Number(array[i]) !== array[i]) throw ("I can do it with digits only");
+        if(Number(array[i]) !== array[i]) throw "I can do it with digits only";
         const filteredArray = array.filter((item, j) => j !== i);
             result[i] = filteredArray.reduce((sum, current) => sum + current);
         }
