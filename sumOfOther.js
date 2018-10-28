9fbb1d5d5ab4d7d6602d26045b28fc61e87d9332
vovakor1 <v.korotkin111gmail.com>
2018-10-28 20:49:01 +0300

    refactor: change let to const for 'result' array

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,8 +1,8 @@
 function sumOfOther(array) {
-    let result = [];
+    const result = [];
     const LENGTH = array.length;
-    for (let i = 0; i < LENGTH; i++){
-        if(Number(array[i]) !== array[i]) throw "I can do it with digits only";
+    for (let i = 0; i < LENGTH; i++) {
+        if (Number(array[i]) !== array[i]) throw "I can do it with digits only";
         const filteredArray = array.filter((item, j) => j !== i);
         result[i] = filteredArray.reduce((sum, current) => sum + current);
     }
