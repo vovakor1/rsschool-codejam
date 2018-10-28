2deb384fd9029ba4a150e44121d8963158b3c690
vovakor1 <v.korotkin111gmail.com>
2018-10-28 20:37:33 +0300

    refactor: add constant LENGTH

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,9 +1,10 @@
 function sumOfOther(array) {
     let result = [];
-    for (let i = 0; i < array.length; i++){
+    const LENGTH = array.length;
+    for (let i = 0; i < LENGTH; i++){
         if(Number(array[i]) !== array[i]) throw "I can do it with digits only";
         const filteredArray = array.filter((item, j) => j !== i);
-            result[i] = filteredArray.reduce((sum, current) => sum + current);
-        }
-    return console.log(result);
+        result[i] = filteredArray.reduce((sum, current) => sum + current);
+    }
+    return result;
 }
